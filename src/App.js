8b835f761471ce7a52a7dddeb0a4db42f6fbd345
Naisuu <julius.cathalina@gmail.com>
2020-04-19 01:32:36 +0200

    setup react app structure, TODO:cleanup

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I know it looks like shit okay, calm down we're fixing this asap.</h1>
+      <div className="navbar">
+        <div className="logo">
+          Kenepa Logo
+        </div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
