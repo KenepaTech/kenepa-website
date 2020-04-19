3a0174a2b51d31850d14bee9d8ce6d364f7af57b
Naisuu <julius.cathalina@gmail.com>
2020-04-19 22:44:07 +0200

    add new background and rearrange main components

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,16 @@ export default class About extends Component {
     render() {
         return (
             <div>
-                
+                <p>
+                Many individuals face technical challenges that prevent them from realising the full potential of their business and/or project. These days, technical skills are essential to build a competitive digital platform, and it is unreasonable to expect one person or a small team to be able to do it all, and current solutions can be very expensive.
+
+This is where KT comes in: We can ensure that you can focus entirely on delivering content for their business and/or project without having to worry about the medium. We create and maintain an online platform specifically catered to your needs.
+
+While there are alternatives for personal website services that don't demand in-depth technical expertise, it still requires significant time and effort while still costing you a monthly fee. Furthermore, the individual may need a more specific solution to truly get the most out of their projects, which we can offer by offering tailor-made solutions to their needs, unlike these site creators.
+
+
+As a young team ourselves, we also understand the need for affordable solutions. To this end, we want to offer our services at the best prices possible, as our focus lies more within gaining experience.
+                </p>
             </div>
         )
     }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Clock from '../components/Clock';
 
+import '../components/Home.scss';
+
 
 export default class Home extends Component {
     render() {
         return (
-            <Clock />
+            <div>
+                <Clock />
+                <section id="landing">
+                    <p>
+                        <p className="welcome">Welcome to Kenepa Tech!</p>
+                        We are a dedicated group of young professionals and students from the ABC-islands
+                        that excel at using a multi-disciplinary approach with the latest stable technologies
+                        to provide customers with innovative solutions for their businesses and/or projects.
+                        Enjoy your stay, and feel free to contact us
+                        with any questions about our services.
+                    </p>
+                </section>
+            </div>
         )
     }
 }
