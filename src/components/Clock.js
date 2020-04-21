d58d515fe7c8e2bd8b54e51de4da441005954e55
Naisuu <julius.cathalina@gmail.com>
2020-04-21 17:37:31 +0200

    refactor greeting code and add bontardi

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import './Clock.scss';
 
+// Greeting block
+
+let hour = new Date().getHours();
+
+let greeting = `Bondia!`;
+
+if (hour > 15 && hour < 19) {
+    greeting = `Bontardi!`;
+}
+
+if (hour >= 19){
+    greeting = `Bonochi!`;
+}
+
 export default class Clock extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
             time: new Date(),
-            hour: new Date().getHours()
+            // hour: new Date().getHours()
         };
     }
 
@@ -28,14 +42,12 @@ export default class Clock extends Component {
         });
     }
 
-    greet() {
-        
-    }
+    
 
     render() {
         return (
             <section className="Time">
-                <h1>{this.state.hour < 19 ? `Bondia!` : `Bonochi!` }</h1>
+                <h1>{greeting}</h1>
                 <h2>{this.state.time.toLocaleTimeString()}</h2>
             </section>
         );
