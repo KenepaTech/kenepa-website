c007196aee2aaf14aad770dda81b83253e39012a
Rocheandley Kwidama <rorobig@live.com>
2020-04-26 13:18:44 +0200

    proper js navbar with hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { ThemeProvider } from 'styled-components';
+import Theme from './Theme';
+import Burger from './components/Burger';
+import Menu from './components/Menu';
 
 function App() {
+  const [ open, setOpen ] = useState(false);
+  console.log(open);
   return (
-    <div className="App">
-        <Navbar />
-        
-        <Footer />
-    </div>
+    <ThemeProvider theme={Theme}>
+      <>
+      <div className="App">
+          <Navbar open={open} setOpen={setOpen}/>
+
+          <Footer />
+      </div>
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -6,72 +6,122 @@ import {
     Link
 } from "react-router-dom";
 import './Navbar.scss';
-import Home from './Home.js';
-import About from './About.js';
+import Home from './Home';
+import About from './About';
+import Burger from './Burger';
+import Menu from './Menu';
+import  { bool, func } from 'prop-types';
 
-    const navSlide = () => {
-        const burger = document.querySelector('.burger');
-        const nav = document.querySelector('.nav-links');
-
-        burger.addEventListener('click',()=>{
-            nav.classList.toggle('nav-active');
-        });
-
-    }
+// FIXME: Has to be available before client can interact with UI
+// const [open,setOpen] = useState(false);
 
 
-export default class Navbar extends Component {
-    render() {
-        return ( 
-
-        //    { navSlide()}
-            <Router>
-                
-                
-            <div >
-            
-            <nav >
-                <div class="logo">
-                    <h4>The nav</h4>
+const Navbar = ({open, setOpen}) => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <div class="logo">
+            <h4>The nav</h4>
+          </div>
+          <ul class="nav-links">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <a href="">Welcome</a>
+            </li>
+            <li>
+              
+            <Link to="/about">About</Link>
+            </li>
+            <li>
+              <a href="">Projects</a>
+            </li>
+          </ul>
+          <div>
+          <Burger  open={open} setOpen={setOpen} />
+          <Menu  open={open} />
+          
         </div>
-                    <ul class="nav-links">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Welcome</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Projects</a></li>
-                    </ul>
-                    <div class="burger">
-                    <div class="line1"></div>    
-                    <div class="line2"></div>    
-                    <div class="line3"></div>    
-                    </div>
-            
-            </nav>
+        </nav>
 
-            
+        
 
-            {
-                /* A <Switch> looks through its children <Route>s and
-                                renders the first one that matches the current URL. */
-            } 
-            <Switch >
-            
-            <Route path = "/about" >
-            
-            <About/>
-            
-            </Route> 
-            <Route path = "/" >
-            
-            <Home/>
-            
-            </Route> 
-            </Switch> 
-            </div> 
-            </Router>
-        );
-    }
-
-
+        {/* A <Switch> looks through its children <Route>s and
+                          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
+
+
+Navbar.propTypes = {
+  open:  bool.isRequired,
+  setOpen: func.isRequired,
+};
+
+export default Navbar;
+
+
+
+// export default class Navbar extends Component {
+//     render() {
+        
+//         return (
+//           <Router>
+//             <div>
+//               <nav>
+//                 <div class="logo">
+//                   <h4>The nav</h4>
+//                 </div>
+//                 <ul class="nav-links">
+//                   <li>
+//                     <Link to="/">Home</Link>
+//                   </li>
+//                   <li>
+//                     <a href="">Welcome</a>
+//                   </li>
+//                   <li>
+                    
+//                   <Link to="/about">About</Link>
+//                   </li>
+//                   <li>
+//                     <a href="">Projects</a>
+//                   </li>
+//                 </ul>
+//                 <div>
+//                 <Burger  open={open} setOpen={setOpen} />
+//                 <Menu  open={open} setOpen={setOpen} />
+                
+//               </div>
+//               </nav>
+
+              
+
+//               {/* A <Switch> looks through its children <Route>s and
+//                                 renders the first one that matches the current URL. */}
+//               <Switch>
+//                 <Route path="/about">
+//                   <About />
+//                 </Route>
+//                 <Route path="/">
+//                   <Home />
+//                 </Route>
+//               </Switch>
+//             </div>
+//           </Router>
+//         );
+//     }
+
+
+// }
 
