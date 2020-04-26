1b77c2f7dc2aae45c240c11afb871e8df9bef3f4
Naisuu <julius.cathalina@gmail.com>
2020-04-26 20:43:57 +0200

    refactor component styling + fix warnings

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,27 +2,20 @@ import React, { Component } from 'react';
 import Clock from '../components/Clock';
 import Textbox from '../components/Textbox';
 
-import '../components/Home.scss';
+import StyledHome from './styles/Home.styled';
 
 
 export default class Home extends Component {
     render() {
         return (
-            <div>
+            <StyledHome>
                 <Clock />
                 <Textbox>
-                    {/* <p className="welcome">Welcome to Kenepa Tech!</p> */}
-                    {/* <p>
-                        We are a dedicated group of young professionals and students from the ABC-islands
-                        that excel at using a multi-disciplinary approach with the latest stable technologies
-                        to provide customers with innovative solutions for their businesses and/or projects.
-                        Enjoy your stay, and feel free to contact us
-                        with any questions about our services.
-                    </p> */}
-                    <img src="https://i.gyazo.com/2e9f4f175dae62e3bbdbff114ac7b817.png"/>
+                    <p className="welcome">Welcome to Kenepa Tech!</p>
+                    <img src="https://i.gyazo.com/2e9f4f175dae62e3bbdbff114ac7b817.png" alt="husky"/>
                 
                 </Textbox>
-            </div>
+            </StyledHome>
         )
     }
 }
---
+++ src/components/styles/Home.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const StyledHome = styled.section`
+    color: white;
+    font-size: 24px;
+`
+
+export default StyledHome;
