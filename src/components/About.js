eb698b0fee776daf14224ae93140f64f486da944
rocheandley <rocheandley@gmail.com>
2020-05-10 00:20:08 +0200

    initial commit for about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-
-
+import StyledAbout, { Grid, Col, Row } from './styles/About.styled';
+import fun from '../img/meeting4.svg'
 export default class About extends Component {
     render() {
         return (
-            <div>
-                <p>
-                Many individuals face technical challenges that prevent them from realising the full potential of their business and/or project. These days, technical skills are essential to build a competitive digital platform, and it is unreasonable to expect one person or a small team to be able to do it all, and current solutions can be very expensive.
-This is where KT comes in: We can ensure that you can focus entirely on delivering content for their business and/or project without having to worry about the medium. We create and maintain an online platform specifically catered to your needs.
-While there are alternatives for personal website services that don't demand in-depth technical expertise, it still requires significant time and effort while still costing you a monthly fee. Furthermore, the individual may need a more specific solution to truly get the most out of their projects, which we can offer by offering tailor-made solutions to their needs, unlike these site creators.
-As a young team ourselves, we also understand the need for affordable solutions. To this end, we want to offer our services at the best prices possible, as our focus lies more within gaining experience.
-                </p>
-            </div>
+            <StyledAbout color="transparent">
+            <h1 > About Us</h1>
+            <Grid>
+                <Row>
+                    <Col size={7}>
+                        <p>  Fugiat cupidatat aliquip ut veniam. Ullamco veniam reprehenderit consequat fugiat et labore dolore irure ea commodo esse. Dolore fugiat id ad in amet deserunt. Reprehenderit nisi sit ullamco enim occaecat et velit dolor amet est nulla ex aliqua. Et sit aute pariatur nisi occaecat esse exercitation in officia et quis elit nulla. Labore ullamco culpa ut et dolor aute ex consequat. Irure nostrud qui laborum incididunt officia sunt proident qui minim id sint anim cupidatat voluptate. Excepteur velit minim nulla labore Lorem quis quis aliqua laboris nulla anim dolore. Elit elit voluptate non adipisicing excepteur ea aliqua duis. Velit minim deserunt labore est in esse consectetur magna sit sunt qui.
+                        Sit nostrud officia incididunt consequat adipisicing. Occaecat adipisicing ipsum deserunt quis. Nulla exercitation deserunt veniam labore. Sunt incididunt est excepteur anim eiusmod aliquip esse nisi. Fugiat aute reprehenderit adipisicing nostrud in magna quis ex ut.
+                        Laboris Lorem et id mollit consectetur reprehenderit Lorem. Sunt anim eu dolor minim nulla elit. Deserunt adipisicing non non ipsum eiusmod nisi.      </p>
+                        {/* Hello */}
+                    </Col>
+                    <Col size={5}>
+                    {/* <img alt="placeholder"  src="https://picsum.photos/500/500" /> */}
+                    <img alt="placeholder"  src={fun} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size={5} collapse="xs">
+                        {/* meh */}
+                    </Col>
+                    <Col size={8}>
+                       {/* alright */}
+                    </Col>
+                </Row>
+
+              
+            </Grid>
+            </StyledAbout>
         )
     }
 }
