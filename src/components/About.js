3d8ffb5a345f5846cf10a2e4f881ec8637814d8a
rocheandley <rocheandley@gmail.com>
2020-05-11 21:54:55 +0200

    started grid from scratch

--- src/components/About.js
+++ src/components/About.js
@@ -5,35 +5,122 @@ import "../tailwind.generated.css";
 export default class About extends Component {
   render() {
     return (
-
       <StyledAbout color="transparent">
         <h1> About Us</h1>
-        <Grid > 
-          <Row className="flex mb-4" >
-            <Col className="w-1/2 bg-gray-400 h-12 sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/2">
-              <p>
-                Ad velit mollit id mollit nostrud elit fugiat amet incididunt voluptate pariatur occaecat fugiat. Officia sit commodo dolore nisi elit qui qui duis culpa incididunt. Tempor sint et ex deserunt irure. Aliqua eu dolore irure non quis nisi ea id. Dolore pariatur incididunt irure commodo duis. Id officia reprehenderit reprehenderit qui magna ut ullamco id reprehenderit et eiusmod do.
-
-Veniam irure qui excepteur cupidatat ut Lorem irure esse velit. Elit aliqua in proident laboris ex cupidatat deserunt nisi eiusmod aliqua. Ut tempor mollit labore velit culpa eu esse. Sunt culpa ad ullamco consectetur nisi Lorem id. Esse eiusmod velit non laboris esse veniam labore irure pariatur tempor laboris et. Fugiat reprehenderit dolore ullamco sint exercitation.
-
-Fugiat occaecat do do ut adipisicing reprehenderit Lorem do labore. Nulla ea occaecat qui laboris duis non. Consectetur amet consectetur deserunt do aliqua cillum est elit id id labore. Et aliquip commodo adipisicing est irure officia ex aliqua officia sint ea enim veniam.
-
-Laboris est ut labore proident Lorem non irure sit Lorem qui sunt adipisicing nulla. Ad in duis do laborum et occaecat. Ullamco ipsum eiusmod officia occaecat ullamco anim amet laboris minim ad ut do laborum occaecat.
-              </p>
-            </Col>
-            <Col className=" w-1/2 bg-gray-500 h-12 sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/2">
-            <p> 
-                banda rechts
-       <img alt="placeholder" src={logo} />
-
+        {/* <Row className="flex flex-wrap -mx-4 mb-4">
+          <Col className="mb-4 md:mb-0 px-4 w-1/2 bg-gray-800 h-12 sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/3">
+            <p>
+             
             </p>
-            </Col >
-          </Row>
-          <Row>
-            <Col></Col>
-            <Col></Col>
-          </Row>
-        </Grid>
+          </Col>
+          <Col className="mb-4 md:mb-0 px-4 w-1/2 bg-gray-600 h-12 sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/3">
+            <p>
+              banda mei mei
+              <img alt="placeholder" src={logo} />
+            </p>
+          </Col>
+          <Col className="mb-4 md:mb-0 px-4 w-1/2 bg-gray-500 h-12 sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/3">
+            <p>
+              banda rechts
+              <img alt="placeholder" src={logo} />
+            </p>
+          </Col>
+        </Row> */}
+        {/* <Row className="flex flex-wrap -mx-4 mb-4">
+          <Col className=" px-2 w-1/2 md:w-1/3 lg:w-1/6">
+            <img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+          </Col>
+          <Col className=" px-2 w-1/2 md:w-1/3 lg:w-1/6">
+            <img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+          </Col>
+          <Col className=" px-2 w-1/2 md:w-1/3 lg:w-1/6">
+            <img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+          </Col>
+          
+         
+        </Row>
+
+
+        <Row className="flex flex-wrap -mx-4 mb-4">
+          <Col className=" px-2 w-1/2 md:w-1/3 lg:w-1/6">
+            <img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+          </Col>
+          <Col className=" px-2 w-1/2 md:w-1/3 lg:w-1/6">
+            <img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+          </Col>
+          <Col className=" px-2 w-1/2 md:w-1/3 lg:w-1/6">
+            <img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+          </Col>
+          
+         
+        </Row> */}
+
+        <div class="flex flex-wrap -mx-3  sm:-mx-3 md:-mx-1 lg:-mx-2 xl:-mx-2">
+          <div class="my-3 px-3 w-1/2  bg-gray-800 sm:my-3 sm:px-3 sm:w-full md:my-1 md:px-1 md:w-1/3 ">
+            <p>
+              Reprehenderit id aliqua ex esse esse dolore laboris nulla Lorem
+              nisi laboris ea. Do dolore nisi ipsum tempor sit sunt dolor id.
+              Consequat commodo ad ipsum dolore reprehenderit aliqua nostrud
+              minim excepteur laboris consectetur. Excepteur qui labore sunt
+              labore et laborum. Do nisi ad reprehenderit tempor fugiat.
+              Occaecat cillum dolore do pariatur aliqua. Cillum esse commodo
+              ullamco incididunt magna officia sit laborum tempor Lorem dolor
+              incididunt in. Consectetur eu deserunt voluptate proident qui anim
+              ad cillum aliqua velit ea est pariatur. Nisi non id sunt Lorem
+              amet velit anim laborum occaecat officia enim id voluptate
+              laboris. Sint non ipsum exercitation aliqua proident duis officia
+              consequat ut exercitation labore tempor dolore ipsum. Aliqua ipsum
+              laborum reprehenderit eu cupidatat velit. Et veniam culpa cillum
+              reprehenderit sunt labore anim quis. In ad magna aliquip in
+              reprehenderit anim cupidatat ex eu occaecat sunt. Mollit ut non id
+              Lorem esse dolore exercitation velit Lorem mollit irure magna est
+              ut. Ad magna laborum reprehenderit proident. 
+            </p>
+          </div>
+
+          <div class="my-3 px-3 w-1/3  bg-gray-600 sm:my-3 sm:px-3 sm:w-full md:my-1 md:px-1 md:w-1/3 ">
+            <p>I Am Groot!</p>
+            <img alt="placeholder" style={{borderRadius: '50%'}} className="block justify-center h-auto w-full" src="https://picsum.photos/1820" />
+          </div>
+
+          <div class="my-3 px-3 w-1/3   bg-gray-700 sm:my-3 sm:px-3 sm:w-full md:my-1 md:px-1 md:w-1/3  ">
+            <p>
+              Reprehenderit id aliqua ex esse esse dolore laboris nulla Lorem
+              nisi laboris ea. Do dolore nisi ipsum tempor sit sunt dolor id.
+              Consequat commodo ad ipsum dolore reprehenderit aliqua nostrud
+              minim excepteur laboris consectetur. Excepteur qui labore sunt
+              labore et laborum. Do nisi ad reprehenderit tempor fugiat.
+              Occaecat cillum dolore do pariatur aliqua. Cillum esse commodo
+              ullamco incididunt magna officia sit laborum tempor Lorem dolor
+              incididunt in. Consectetur eu deserunt voluptate proident qui anim
+              ad cillum aliqua velit ea est pariatur. Nisi non id sunt Lorem
+              amet velit anim laborum occaecat officia enim id voluptate
+              laboris. Sint non ipsum exercitation aliqua proident duis officia
+              consequat ut exercitation labore tempor dolore ipsum. Aliqua ipsum
+              laborum reprehenderit eu cupidatat velit. Et veniam culpa cillum
+              reprehenderit sunt labore anim quis. In ad magna aliquip in
+              reprehenderit anim cupidatat ex eu occaecat sunt. Mollit ut non id
+              Lorem esse dolore exercitation velit Lorem mollit irure magna est
+              ut. 
+            </p>
+          </div>
+        </div>
+
+        {/* <div class="flex flex-wrap -mx-3  sm:-mx-3 md:-mx-1 lg:-mx-2 xl:-mx-2">
+
+<div class="my-3 px-3 w-1/2  sm:my-3 sm:px-3 sm:w-full md:my-1 md:px-1 md:w-1/3 ">
+<img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+</div>
+
+<div class="my-3 px-3 w-1/3 sm:my-3 sm:px-3 sm:w-full md:my-1 md:px-1 md:w-1/3 ">
+<img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+</div>
+
+<div class="my-3 px-3 w-1/3  sm:my-3 sm:px-3 sm:w-full md:my-1 md:px-1 md:w-1/3  ">
+<img alt="placeholder" style={{borderRadius: '50%'}} className="block h-auto w-full" src="https://picsum.photos/300" />
+</div>
+
+</div> */}
       </StyledAbout>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "./tailwind.generated.css";
 
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
