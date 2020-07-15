4469ba683f863cb29fee939372f0c345cb8e4b1b
naisuu <julius.cathalina@gmail.com>
2020-07-15 20:52:28 +0200

    add base for projects page

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
               showCloseIcon={true}
               >
 
-                <Menu />
+                <Menu  open/>
 
             </Modal>
 
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+export default class Projects extends Component {
+    render() {
+        return(
+            <div>
+                HEY THERE!
+                TODO: make api calls to our public github repos?
+            </div>
+        )
+    }
+}
